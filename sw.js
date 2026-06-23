7a4ea76eb512a6e2fa582f1303c187bbe42ed465
EmileDaccache12 <160343152+EmileDaccache12@users.noreply.github.com>
2026-06-23 08:32:00 +0300

    Enhance service worker with cache management
    Refactor service worker to include cache activation and skip waiting.

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("tic-tac-toe-v1.1").then(cache => {
+const CACHE_NAME = "tic-tac-toe-v1.1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -10,10 +14,26 @@ self.addEventListener("install", e => {
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+
+      const keys = await caches.keys();
+      await Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+
+      await self.clients.claim();
+    })()
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
